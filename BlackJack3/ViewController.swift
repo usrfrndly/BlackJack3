77100a30c142f9db62d4ccd116884170f9835975
jax <jackiehorowitz@mac.com>
2014-10-29 16:30:16 -0400

    Initial Commit

---
+++ BlackJack3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackJack3
+//
+//  Created by Jaclyn May on 10/29/14.
+//  Copyright (c) 2014 NYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
